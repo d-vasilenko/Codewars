4d891c5af5af4ad192139c75b5a69555bb5bc743
Denis Vasilenko <ghostinchip@gmail.com>
2025-01-23 09:38:00 +0300

    8-for-ufc-fans.js is done

---
+++ 8-for-ufc-fans.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function quote(fighter) {
+  switch (fighter.toLowerCase()) {
+    case 'george saint pierre':
+      return 'I am not impressed by your performance.';
+    case 'conor mcgregor':
+      return 'I\'d like to take this chance to apologize.. To absolutely NOBODY!';
+  }
+};
+
