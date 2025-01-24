6f52c91e0cbda78acfee3b75436295078da7004e
Denis Vasilenko <ghostinchip@gmail.com>
2025-01-24 11:11:07 +0300

    8 Who ate the cookie?

---
+++ 8-who-ate-the-cookie.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//who = t=="string" ? 'Zach' : t=="number" ? 'Monica' : 'the dog'
+
+function cookie(x){
+  if (typeof x === 'string') {
+    return "Who ate the last cookie? It was Zach!";
+  } else if (typeof x === 'number') {
+    return "Who ate the last cookie? It was Monica!";
+  } else {
+    return "Who ate the last cookie? It was the dog!";
+  }
+
+}
